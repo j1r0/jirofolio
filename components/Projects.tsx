3df66d5edea0b4d2b1c2aa9d7df9c6c895306c68
Jim Pamplona <48pamplona@gmail.com>
2024-07-21 10:22:59 -0400

    Projects cards

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { CardBody, CardContainer, CardItem } from './ui/3DCard'
+import { projects } from '../data'
 import Link from 'next/link'
+import { HoverBorderGradient } from './ui/HoverBorderGradient'
 
 const Projects = () => {
   return (
-    <div className='py-20 '>
+    <div className='pt-20 '>
       <div>
         <h1 className='text-4xl font-bold text-center'>
           Some of my {" "}
@@ -12,50 +14,71 @@ const Projects = () => {
             Projects
           </span>
         </h1>
-        <CardContainer className="inter-var">
+      </div>
+      <div className="flex flex-row gap-4 flex-wrap justify-center mx-10 py-10">
+        {projects.map((item) => (
+        <CardContainer className="inter-var " key={item.id}>
       <CardBody className=" relative group/card  dark:hover:shadow-2xl dark:hover:shadow-black-400/[0.1] dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          {item.title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          {item.des}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <img
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={item.img}
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
-        <div className="flex justify-between items-center mt-20">
+        {/* Icons */}
+        <div className="flex justify-between items-center">
           <CardItem
             translateZ={20}
-            as={Link}
-            href="https://twitter.com/mannupaaji"
             target="__blank"
             className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
           >
-            Try now →
+                          <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {item.iconLists.map((icon, index) => (
+                    <div
+                      key={index}
+                      className="border bg-black border-white/[.2] rounded-full background-blur-md lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt="icon5" className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                </div>
           </CardItem>
           <CardItem
             translateZ={20}
-            as="button"
-            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+            className=" mt-7 mb-3 rounded-xl text-white text-xs font-bold"
           >
-            Sign up
+                  <HoverBorderGradient
+        as="button"
+        className="bg-transparent text-black dark:text-white flex items-center space-x-2"
+      >
+        <span>Check it out</span>
+      </HoverBorderGradient>
           </CardItem>
         </div>
       </CardBody>
     </CardContainer>
+        )) }
 
       </div>
 
